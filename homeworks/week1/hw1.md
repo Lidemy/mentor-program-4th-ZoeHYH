2021638e1da157e8f3cd8429be6cc420348e97cd
ZoeHoTW <31921198+105405161@users.noreply.github.com>
2020-07-14 16:43:18 +0800

    week1 done

--- homeworks/week1/hw1.md
+++ homeworks/week1/hw1.md
@@ -1,2 +1,9 @@
 ## 交作業流程
-
+1. 將專案複製到本地電腦 `git clone 網址`
+2. 在專案資料夾裡，每週建立一個新分支 `git branch week?`
+3. 移到新分支 `git checkout week?`，修改檔案，撰寫作業
+4. 將新作業加入版本控制 `git add .`
+5. 將暫存的檔案提交 `git commit -m"week? 完成"`
+6. 將分支推上雲端 `git push -u origin week?`
+7. 在 GitHub 發出 Pull request
+8. 助教審核通過後會 `merge` 到 `master`，再把 `master` `pull` 下到本地的 `master` 並刪除 `week?` 分支
---
+++ homeworks/week1/超級挑戰題.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#read -p "GitHub username: " name
+#str=$(curl -s https://api.github.com/users/)
+#$str"${i}" | grep '\"name\"' | cut -d'"' -f '4' 
+curl -s https://api.github.com/users/${1} | grep '\"name\"' | cut -d'"' -f 4
+curl -s https://api.github.com/users/${1} | grep '\"bio\"' | cut -d'"' -f 4
+curl -s https://api.github.com/users/${1} | grep '\"location\"' | cut -d'"' -f 4
+curl -s https://api.github.com/users/${1} | grep '\"blog\"' | cut -d'"' -f 4 
