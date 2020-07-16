752e69090b70389b91d4d2f907a260e2d214c450
ZoeHoTW <31921198+105405161@users.noreply.github.com>
2020-07-17 00:13:25 +0800

    0

--- homeworks/week3/hw4.js
+++
@@ -1,8 +0,0 @@
-function palindrome(s) {
-  let r = '';
-  for (let i = s.length - 1; i > -1; i -= 1) r += s[i];
-  console.log(s === r ? 'True' : 'False');
-}
-
-palindrome('abbbba');
-palindrome('ac');
