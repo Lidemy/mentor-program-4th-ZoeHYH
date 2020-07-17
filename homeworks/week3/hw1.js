9a1c5a79807c3071d1267da0cd0b0fa262a85104
ZoeHoTW <31921198+105405161@users.noreply.github.com>
2020-07-18 00:20:05 +0800

    Revised

--- homeworks/week3/hw1.js
+++ homeworks/week3/hw1.js
@@ -2,7 +2,9 @@ function printStar(num) {
   const stars = [];
   for (let i = 0; i < Number(num); i += 1) {
     stars[i] = '';
-    for (let j = 0; j < i + 1; j += 1) stars[i] += '*';
+    for (let j = 0; j < i + 1; j += 1) {
+      stars[i] += '*';
+    }
     console.log(stars[i]);
   }
 }
--- homeworks/week3/hw5.js
+++ homeworks/week3/hw5.js
@@ -1,5 +1,6 @@
 function compare(a, b, k) {
-  const [m, n] = [a.toString(10), b.toString(10)];
+  const m = a.toString(10);
+  const n = b.toString(10);
   let result = '';
   if (m === n) result = 'DRAW';
   if (m.length > n.length) result = k === 1 ? 'A' : 'B';
@@ -8,6 +9,7 @@ function compare(a, b, k) {
     for (let i = 0; i < m.length; i += 1) {
       if (Number(m[i]) > Number(n[i])) result = k === 1 ? 'A' : 'B';
       else if (Number(m[i]) < Number(n[i])) result = k === 1 ? 'B' : 'A';
+      if (result) return console.log(result);
     }
   }
   return console.log(result);
@@ -16,3 +18,6 @@ function compare(a, b, k) {
 compare(1, 2, 1);
 compare(1, 2, -1);
 compare(2, 2, 1);
+compare(19, 20, -1);
+compare(1, 20, 1);
+compare(1, 20, -1);
