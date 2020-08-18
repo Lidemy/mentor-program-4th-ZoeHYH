3c3e789658ad5fdc1e36d7d77b52f73fd373ec14
ZoeHYH <zoehyhtw@gmail.com>
2020-08-18 22:07:36 +0800

    week9 done

---
+++ homeworks/week9/hw1/do_login.php
@@ -0,0 +1,27 @@
+<?php
+    session_start();
+    require_once('./conn.php');
+
+    if (empty($_POST['username']) || empty($_POST['password'])) {
+        header("Location: ./login.php?errcode=1");
+        exit();
+    }
+
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+
+    $sql = sprintf('SELECT `username`, `password` FROM `ZoeHYH_users` WHERE `username` = "%s" AND `password` = "%s"',
+        $username, $password);
+    $result = $conn->query($sql);
+    if (!$result) {
+        header("Location: ./login.php?errcode=3");
+        exit();
+    }
+    if (!$result->num_rows) {
+        header("Location: ./login.php?errcode=2");
+        exit();
+    }
+
+    $_SESSION['username'] = $username;
+    header('Location: ./index.php');
+?>
