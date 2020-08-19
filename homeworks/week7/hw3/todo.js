0b84c0accc76a5be87b6b9d7e3ba3fbe0da0159e
ZoeHYH <zoehyhtw@gmail.com>
2020-08-19 17:41:54 +0800

    week3 hw3 revised

--- homeworks/week7/hw3/todo.js
+++ homeworks/week7/hw3/todo.js
@@ -1,10 +1,11 @@
 document.querySelector('input[id = "task"]').addEventListener('keydown', (e) => {
     if (e.key === 'Enter' && e.target.value !== '') {
         let li = document.createElement('li');
+        li.classList.add('task');
         li.innerHTML= `
-            <button class="done">Done<img src="" alt=""></button>
-            <p>${e.target.value}</p>
-            <button class="delete">Delete<img src="" alt=""></button>
+            <i class="material-icons done">check_box_outline_blank</i>
+            <p>${escapeHtml(e.target.value)}</p>
+            <i class="material-icons delete">delete</i>
         `
         e.target.parentNode.after(li);
         e.target.value = '';
@@ -14,8 +15,22 @@ document.querySelector('input[id = "task"]').addEventListener('keydown', (e) => 
 document.querySelector('.todo').addEventListener('click', (e) => {
     if (e.target.classList.contains('done')) {
         e.target.parentNode.classList.toggle('finished');
+        if (e.target.parentNode.classList.contains('finished')) {
+            e.target.innerText = 'check_box';
+        } else {
+            e.target.innerText = 'check_box_outline_blank';
+        }
     }
     if (e.target.classList.contains('delete')) {
         e.target.parentNode.remove();
     }
 })
+
+function escapeHtml(content) {
+    return content
+        .replace(/&/g, "&amp;")
+        .replace(/</g, "&lt;")
+        .replace(/>/g, "&gt;")
+        .replace(/"/g, "&quot;")
+        .replace(/'/g, "&#039;");
+}
