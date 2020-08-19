6455c907534ca416379cf514a91d414289045edf
ZoeHYH <zoehyhtw@gmail.com>
2020-08-20 00:17:55 +0800

    week8 hw2 reconstruction

--- homeworks/week8/hw2/twitch.js
+++ homeworks/week8/hw2/twitch.js
@@ -3,8 +3,12 @@ const streamsNumber = 20;
 let offsetNumber = 0;
 const tabs = document.querySelector('.games')
 const title = document.querySelector('.streams h2');
+const api = 'https://api.twitch.tv/kraken/';
+let gameName = '';
 
-function loadSwitchAPI(request, callback) {
+function loadSwitchAPI(api, callback) {
+  const request = new XMLHttpRequest();
+  request.open('GET', api, true);
   request.onload = () => {
     if (request.status >= 200 && request.status < 400) {
       callback(JSON.parse(request.responseText));
@@ -19,38 +23,28 @@ function loadSwitchAPI(request, callback) {
 }
 
 /* make page */
-function loadPage() {
-  const request = new XMLHttpRequest()
-  request.open('GET', `https://api.twitch.tv/kraken/games/top?limit=${gamesNumber}`, true)
-  loadSwitchAPI(request, makePage);
-}
 
 function makePage(json) {
-  let gameNames = json.top.map(a => a.game.name)
+  let gameNames = json.top.map(obj => obj.game.name)
   for (let i = 0; i < gamesNumber; i += 1) {
     tabs.innerHTML += `<button onclick="tabbing(event)">${gameNames[i]}</button>`;
   }
-  let firstButton = tabs.querySelector('button');
-  firstButton.classList.add('active');
-  title.innerText = firstButton.innerText;
-  loadStreams(firstButton.innerText);
+  let defaultButton = tabs.querySelector('button');
+  defaultButton.classList.add('active');
+  gameName = defaultButton.innerText;
+  title.innerText = gameName;
+  loadSwitchAPI(`${api}streams/?game=${encodeURIComponent(gameName)}&limit=${streamsNumber}&offset=${offsetNumber}${api}streams/?game=${encodeURIComponent(gameName)}&limit=${streamsNumber}&offset=${offsetNumber}`, makeStreams);
 }
 
 /* make streams */
-function loadStreams(gameName) {
-  const request = new XMLHttpRequest();
-  request.open('GET', `https://api.twitch.tv/kraken/streams/?game=${gameName}&limit=${streamsNumber}&offset=${offsetNumber}`, true);
-  loadSwitchAPI(request, makeStreams);
-}
-
 function makeStreams(json) {
   let streams = document.querySelector('.streams .container');
-  for (let i = 0; i < json.streams.length; i += 1) {
+  for (stream of json.streams) {
     streams.innerHTML += `
-    <a href="${json.streams[i].channel.url}" class="stream">
-      <img src="${json.streams[i].preview.medium}" alt="live stream">
-      <h3>${json.streams[i].channel.status}</h3>
-      <h4>${json.streams[i].channel.name}</h4>
+    <a href="${stream.channel.url}" class="stream">
+      <img src="${stream.preview.medium}" alt="live stream">
+      <h3>${stream.channel.status}</h3>
+      <h4>${stream.channel.name}</h4>
     </a>`
   }
   if (offsetNumber === 0) {
@@ -62,19 +56,20 @@ function makeStreams(json) {
   }
 }
 
-loadPage();
+loadSwitchAPI(`${api}games/top?limit=${gamesNumber}`, makePage);
 
-/* tab functions */
-function tabbing(event) {
+/* functions */
+function tabbing(e) {
   document.querySelectorAll('.games button').forEach(btn => btn.classList.remove('active'));
-  event.currentTarget.classList.add('active');
-  title.innerText = event.currentTarget.innerText;
+  e.target.classList.add('active');
   document.querySelector('.streams .container').innerHTML = '';
+  gameName = e.target.innerText;
+  title.innerText = gameName;
   offsetNumber = 0;
-  loadStreams(event.currentTarget.innerText);
+  loadSwitchAPI(`${api}streams/?game=${encodeURIComponent(gameName)}&limit=${streamsNumber}&offset=${offsetNumber}${api}streams/?game=${encodeURIComponent(gameName)}&limit=${streamsNumber}&offset=${offsetNumber}`, makeStreams);
 }
 
 function loadMore() {
   offsetNumber += 20;
-  loadStreams(title.innerText);
+  loadSwitchAPI(`${api}streams/?game=${encodeURIComponent(gameName)}&limit=${streamsNumber}&offset=${offsetNumber}${api}streams/?game=${encodeURIComponent(gameName)}&limit=${streamsNumber}&offset=${offsetNumber}`, makeStreams);
 }
